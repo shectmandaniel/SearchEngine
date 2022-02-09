90c4c9eafe922c554495b45ac4e118dad9084a47
Niv Itzhaky <niv.itzhaky@gmail.com>
2022-02-09 15:22:03 +0200

    with algo

--- README.md
+++ README.md
@@ -52,3 +52,53 @@ public class AppController {
 http://localhost:8080/swagger-ui.html#
 <br>
 commit - with swagger
+### Algo
+```
+		<dependency>
+			<!-- jsoup HTML parser library @ https://jsoup.org/ -->
+			<groupId>org.jsoup</groupId>
+			<artifactId>jsoup</artifactId>
+			<version>1.13.1</version>
+		</dependency>
+
+		<dependency>
+			<groupId>joda-time</groupId>
+			<artifactId>joda-time</artifactId>
+			<version>2.10.13</version>
+		</dependency>
+
+		<dependency>
+			<groupId>com.fasterxml.jackson.datatype</groupId>
+			<artifactId>jackson-datatype-joda</artifactId>
+			<version>2.12.3</version>
+		</dependency>
+```
+apply patch - algo
+<br>
+controller/AppController.java
+```java
+    private static final int ID_LENGTH = 6;
+    private Random random = new Random();
+    @Autowired
+    Crawler crawler;
+
+    @RequestMapping(value = "/crawl", method = RequestMethod.POST)
+    public CrawlStatusOut crawl(@RequestBody CrawlerRequest request) throws IOException, InterruptedException {
+        String crawlId = generateCrawlId();
+        if (!request.getUrl().startsWith("http")) {
+            request.setUrl("https://" + request.getUrl());
+        }
+        CrawlStatus res = crawler.crawl(crawlId, request);
+        return CrawlStatusOut.of(res);
+    }
+
+    private String generateCrawlId() {
+        String charPool = "ABCDEFHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < ID_LENGTH; i++) {
+            res.append(charPool.charAt(random.nextInt(charPool.length())));
+        }
+        return res.toString();
+    }
+```
+commit - with algo
--- src/main/java/com/handson/searchengine/controller/AppController.java
+++ src/main/java/com/handson/searchengine/controller/AppController.java
@@ -1,16 +1,39 @@
 package com.handson.searchengine.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.handson.searchengine.crawler.Crawler;
+import com.handson.searchengine.model.CrawlStatus;
+import com.handson.searchengine.model.CrawlStatusOut;
+import com.handson.searchengine.model.CrawlerRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/api")
 public class AppController {
+    private static final int ID_LENGTH = 6;
+    private Random random = new Random();
+    @Autowired
+    Crawler crawler;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(@RequestParam String hello) {
-        return "hello";
+    @RequestMapping(value = "/crawl", method = RequestMethod.POST)
+    public CrawlStatusOut crawl(@RequestBody CrawlerRequest request) throws IOException, InterruptedException {
+        String crawlId = generateCrawlId();
+        if (!request.getUrl().startsWith("http")) {
+            request.setUrl("https://" + request.getUrl());
+        }
+        CrawlStatus res = crawler.crawl(crawlId, request);
+        return CrawlStatusOut.of(res);
+    }
+
+    private String generateCrawlId() {
+        String charPool = "ABCDEFHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < ID_LENGTH; i++) {
+            res.append(charPool.charAt(random.nextInt(charPool.length())));
+        }
+        return res.toString();
     }
 }
