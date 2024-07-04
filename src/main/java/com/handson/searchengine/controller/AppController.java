ad025d5aa9d0e2644cd68d5974f4d636da0a78ec
shectmandaniel <shechtmandaniel@gmail.com>
2024-07-04 14:37:56 +0300

    Testing branch

--- src/main/java/com/handson/searchengine/controller/AppController.java
+++ src/main/java/com/handson/searchengine/controller/AppController.java
@@ -9,6 +9,6 @@ public class AppController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        return "Hello";
+        return "Hello Again";
     }
 }
