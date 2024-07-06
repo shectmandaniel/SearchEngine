a0c264f224bb38a19568d68acc4f081e72dab400
shectmandaniel <shechtmandaniel@gmail.com>
2024-07-06 11:00:30 +0300

    Integrating ElasticSearch

--- src/main/java/com/handson/searchengine/crawler/Crawler.java
+++ src/main/java/com/handson/searchengine/crawler/Crawler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.handson.searchengine.kafka.Producer;
 import com.handson.searchengine.model.*;
+import com.handson.searchengine.util.ElasticSearch;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
@@ -34,6 +35,9 @@ public class Crawler {
     @Autowired
     Producer producer;
 
+    @Autowired
+    ElasticSearch elasticSearch;
+
     protected final Log logger = LogFactory.getLog(getClass());
 
 
@@ -49,6 +53,7 @@ public class Crawler {
         setCrawlStatus(crawlId, CrawlStatus.of(rec.getDistance(), rec.getStartTime(), 0, stopReason));
         if (stopReason == null){
             Document webPageContent = Jsoup.connect(rec.getUrl()).get();
+            indexElasticSearch(rec, webPageContent);
             List<String> innerUrls = extractWebPageUrls(rec.getBaseUrl(), webPageContent);
             addUrlsToQueue(rec, innerUrls, rec.getDistance() +1);
         }
@@ -110,6 +115,13 @@ public class Crawler {
         CrawlStatus cs = om.readValue(redisTemplate.opsForValue().get(crawlId + ".status").toString(),CrawlStatus.class);
         cs.setNumPages(getVisitedUrls(crawlId));
         return CrawlStatusOut.of(cs);
+    }
+
+    private void indexElasticSearch(CrawlerRecord rec, Document webPageContent) {
+        logger.info(">> adding elastic search for webPage: " + rec.getUrl());
+        String text = String.join(" ", webPageContent.select("a[href]").eachText());
+        UrlSearchDoc searchDoc = UrlSearchDoc.of(rec.getCrawlId(), text, rec.getUrl(), rec.getBaseUrl(), rec.getDistance());
+        elasticSearch.addData(searchDoc);
     }
 
 
